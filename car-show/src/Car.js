ecb63dedfabdceddcb7576ddb3884a74232bea4b
Nickolas Theodoulou <nicktheodoulou96@gmail.com>
2023-08-12 10:51:38 +0100

    add ability to have 2 different cars

--- car-show/src/Car.js
+++ car-show/src/Car.js
@@ -3,17 +3,33 @@ import { useFrame, useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { Mesh } from "three";
 
-// based on "Chevrolet Corvette (C7)" (https://sketchfab.com/3d-models/chevrolet-corvette-c7-2b509d1bce104224b147c81757f6f43a) 
+const cars = [
+  {
+    url: "models/car/corvette/scene.gltf",
+    scale: 0.005,
+    yPosition: -0.035,
+  },
+  {
+    url: "models/car/bmw-e30/scene.gltf",
+    scale: 1,
+    yPosition: 0,
+  }
+]
+// based on "Chevrolet Corvette (C7)" (https://sketchfab.com/3d-models/chevrolet-corvette-c7-2b509d1bce104224b147c81757f6f43a)
 // by Martin Trafas (https://sketchfab.com/Bexxie) licensed under CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)
 export function Car() {
+  const [corvette, bmw] = cars
+  const car = corvette
+
+
   const gltf = useLoader(
     GLTFLoader,
-    process.env.PUBLIC_URL + "models/car/scene.gltf"
+    process.env.PUBLIC_URL + car.url
   );
-  
+
   useEffect(() => {
-    gltf.scene.scale.set(0.005, 0.005, 0.005);
-    gltf.scene.position.set(0, -0.035, 0);
+    gltf.scene.scale.set(car.scale, car.scale, car.scale);
+    gltf.scene.position.set(0, car.yPosition, 0);
     gltf.scene.traverse((object) => {
       if (object instanceof Mesh) {
         object.castShadow = true;
@@ -26,11 +42,14 @@ export function Car() {
   useFrame((state, delta) => {
     let t = state.clock.getElapsedTime();
 
-    let group = gltf.scene.children[0].children[0].children[0];
-    group.children[0].rotation.x = t * 2;
-    group.children[2].rotation.x = t * 2;
-    group.children[4].rotation.x = t * 2;
-    group.children[6].rotation.x = t * 2;
+
+    if(car === corvette) {
+      let group = gltf.scene.children[0].children[0].children[0];
+      group.children[0].rotation.x = t * 2;
+      group.children[2].rotation.x = t * 2;
+      group.children[4].rotation.x = t * 2;
+      group.children[6].rotation.x = t * 2;
+    }
   });
 
   return <primitive object={gltf.scene} />;
